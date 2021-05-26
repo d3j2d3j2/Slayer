6e86c9fa7a455ff47c77ff43bbe46565d4488c67
d3j2d3j2 <013j2@naver.com>
2021-05-27 01:28:14 +0900

    기사(탱커)의 이동과 공격

---
+++ Assets/LeeJeonggeun/CamRotate.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamRotate : MonoBehaviour
+{
+    public float rotSpeed = 1000;
+    float mx;
+    float my;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Mouse X");
+        float v = Input.GetAxis("Mouse Y");
+
+        mx += h * rotSpeed * Time.deltaTime;
+        /*
+        my += v * rotSpeed * Time.deltaTime;
+        
+        if(my >= 90)
+        {
+            my = 90;
+        }
+        else if (my <= -90)
+        {
+            my = -90;
+        }
+        */
+        transform.eulerAngles = new Vector3(-my, mx, 0);
+    }
+}
---
+++ Assets/LeeJeonggeun/PlayerAttack.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private InputMovement input; // 플레이어 입력을 알려주는 컴포넌트
+    private Rigidbody playerRigidbody; // 플레이어 캐릭터의 리지드바디
+    private Animator playerAnimator; // 플레이어 캐릭터의 애니메이터
+    private bool attacking;
+    private bool attacked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        input = GetComponent<InputMovement>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
+        attacking = false;
+    }
+
+    private void FixedUpdate()
+    {
+        //입력값에 따라 애니메이터의 Move 파라미터값 변경
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Attack();
+    }
+
+    void Attack()
+    {
+        if (input.attack > 0) attacked = true;
+        else attacked = false;
+
+        if (attacking == false && attacked == true) playerAnimator.SetBool("attack", true);
+        else if (attacking == true && attacked == false) playerAnimator.SetBool("idle", true);
+
+        attacking = attacked;
+    }
+}
